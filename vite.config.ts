33a5a35b232325b5b7326bd690f459d2f3b85501
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-08-22 13:03:37 -0500

    chore(vercel): add vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   plugins: [
     react(),
+    vercel(),
     viteStaticCopy({
       targets: [
         {
